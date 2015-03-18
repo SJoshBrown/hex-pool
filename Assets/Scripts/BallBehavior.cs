fdfe4c93090e337cd38bc5eb99df338cdc779793
Josh Brown <S.Josh.Brown@gmail.com>
2015-03-18 16:57:34 -0500

    development

---
+++ Assets/Scripts/BallBehavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Physics.sleepThreshold = 1000.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
--- Assets/Scripts/RenderLines.cs
+++ Assets/Scripts/RenderLines.cs
@@ -7,12 +7,14 @@ public class RenderLines : MonoBehaviour {
 	private Vector3 mouseWorld;
 	public Camera gameCamera;
 	public GameObject sphereCastTestObject;
+	private Vector3 cueBallDirection;
 	// Use this for initialization
 	void Start () {
 		line = gameObject.GetComponent<LineRenderer> ();
 		line.SetWidth(0.1f, 0.1f);
 		line.SetVertexCount(2);
 		//gameCamera = GameObject.FindObjectOfType<Camera> ();
+		Physics.sleepThreshold = 1000.0f;
 
 	}
 	
@@ -25,8 +27,7 @@ public class RenderLines : MonoBehaviour {
 		UpdateLine ();
 	}
 
-
-	//Modified code from http://answers.unity3d.com/questions/184442/drawing-lines-from-mouse-position.html
+	
 	void UpdateLine()
 	{
 
@@ -35,14 +36,16 @@ public class RenderLines : MonoBehaviour {
 		RaycastHit sphereHit;
 		Debug.DrawRay(mouseToWorldSpaceRay.origin, mouseToWorldSpaceRay.direction * 100, Color.yellow);
 		if (Physics.Raycast (mouseToWorldSpaceRay, out hit)) {
-			Vector3 cueBallDirection = (new Vector3(hit.point.x, 0.594f, hit.point.z) - gameObject.transform.position);
-			sphereCastTestObject.transform.position = new Vector3(hit.point.x, 0.594f, hit.point.z);
-			if(Physics.SphereCast(this.gameObject.transform.position,0.5f, -cueBallDirection, out sphereHit))
-			{
-			
+			cueBallDirection = (new Vector3(hit.point.x, this.gameObject.transform.position.y, hit.point.z) - gameObject.transform.position).normalized;
+			if(Physics.SphereCast(this.gameObject.transform.position,0.25f, -cueBallDirection, out sphereHit))
+			{			
 				line.SetPosition(0,this.gameObject.transform.position);
 				line.SetPosition(1,sphereHit.point);
 			}
+		}
+		if (Input.GetMouseButtonDown(0)) {
+			this.gameObject.GetComponent<Rigidbody> ().AddForce (new Vector3(-cueBallDirection.x,0,-cueBallDirection.z) * 5000.0f);
+			Debug.Log (cueBallDirection.y);
 		}
 
 	}
