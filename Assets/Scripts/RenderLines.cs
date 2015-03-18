308e46af84386bf116e5795ff3d29ebe3ad60980
Josh Brown <S.Josh.Brown@gmail.com>
2015-03-18 14:00:24 -0500

    draw line from cueBall in desired direction

--- Assets/Scripts/RenderLines.cs
+++ Assets/Scripts/RenderLines.cs
@@ -9,16 +9,16 @@ public class RenderLines : MonoBehaviour {
 	public GameObject sphereCastTestObject;
 	// Use this for initialization
 	void Start () {
-		//line = gameObject.GetComponent<LineRenderer> ();
-		//line.SetWidth(0.1f, 0.1f);
-		//line.SetVertexCount(2);
+		line = gameObject.GetComponent<LineRenderer> ();
+		line.SetWidth(0.1f, 0.1f);
+		line.SetVertexCount(2);
 		//gameCamera = GameObject.FindObjectOfType<Camera> ();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		mousePosition = Input.mousePosition;
+		//mousePosition = Input.mousePosition;
 		//mousePosition.z = gameObject.transform.position.z;
 		//mouseWorld = gameCamera.ScreenToWorldPoint(mousePosition);
 		//mouseWorld.y = gameObject.transform.position.y;
@@ -30,11 +30,19 @@ public class RenderLines : MonoBehaviour {
 	void UpdateLine()
 	{
 
-		Ray ray = gameCamera.ScreenPointToRay(Input.mousePosition);
+		Ray mouseToWorldSpaceRay = gameCamera.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hit;
-		Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow);
-		if (Physics.Raycast (ray, out hit)) {
-			sphereCastTestObject.transform.position = new Vector3(hit.point.x, gameObject.transform.position.y, hit.point.z);
+		RaycastHit sphereHit;
+		Debug.DrawRay(mouseToWorldSpaceRay.origin, mouseToWorldSpaceRay.direction * 100, Color.yellow);
+		if (Physics.Raycast (mouseToWorldSpaceRay, out hit)) {
+			Vector3 cueBallDirection = (new Vector3(hit.point.x, 0.594f, hit.point.z) - gameObject.transform.position);
+			sphereCastTestObject.transform.position = new Vector3(hit.point.x, 0.594f, hit.point.z);
+			if(Physics.SphereCast(this.gameObject.transform.position,0.5f, -cueBallDirection, out sphereHit))
+			{
+			
+				line.SetPosition(0,this.gameObject.transform.position);
+				line.SetPosition(1,sphereHit.point);
+			}
 		}
 
 	}
