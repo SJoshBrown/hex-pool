1ca09203312e39da2b74fce02ef7c2404105e963
Josh Brown <S.Josh.Brown@Gmail.com>
2015-03-23 19:15:25 -0500

    destroy balls

---
+++ Assets/Scripts/BallDestroyer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallDestroyer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision coll ) {
+		Debug.Log ("collide");
+		Destroy (coll.collider.gameObject);
+	}
+}
